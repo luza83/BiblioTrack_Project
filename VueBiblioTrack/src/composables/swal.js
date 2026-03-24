4e0fb5e2287307d96827059ffb380e246fb844ad
luza83 <luz.a.acosta.hernandez@stud.himolde.no>
2026-03-24 10:40:03 +0100

    added style to feedback modals from sweetalert so they match current theme

--- VueBiblioTrack/src/composables/swal.js
+++ VueBiblioTrack/src/composables/swal.js
@@ -1,6 +1,9 @@
 import Swal from 'sweetalert2'
+import { useThemeStore } from '@/stores/themeStore.js'
 
 export function useSwal() {
+  const themeStore = useThemeStore()
+  const backdropColor = themeStore.theme === 'dark' ? 'rgba(0,0,0,0.8)' : 'rgba(255,255,255,0.8)'
   const showAlert = async (options) => {
     return await Swal.fire(options)
   }
@@ -10,6 +13,8 @@ export function useSwal() {
       position: 'top-end',
       icon: 'success',
       title: message,
+      theme: themeStore.theme,
+      backdrop: backdropColor,
       showConfirmButton: false,
       timer: 1500,
     })
@@ -20,6 +25,8 @@ export function useSwal() {
       position: 'top-end',
       icon: 'error',
       title: message,
+      theme: themeStore.theme,
+      backdrop: backdropColor,
       showConfirmButton: false,
       timer: 1500,
     })
@@ -31,6 +38,8 @@ export function useSwal() {
       text: message,
       icon: 'warning',
       showCancelButton: true,
+      themeStore: themeStore.theme,
+      backdrop: backdropColor,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
       confirmButtonText: 'Yes, delete it!',
@@ -42,29 +51,34 @@ export function useSwal() {
       width: '400px',
       height: 'auto',
       icon: 'success',
+      theme: themeStore.theme,
+      backdrop: backdropColor,
       title: message,
       showConfirmButton: true
     })
   }
 
-const showConfirmBorrowStatus = async (dueDate, onConfirm) => {
-  return await showAlert({
-    title: `This book has been reserved. Due date: ${dueDate}`,
-    text: "Do you want to pick up this book now?",
-    showDenyButton: true,
-    confirmButtonText: "Yes",
-    denyButtonText: "No",
-  }).then(async (result) => {
-    if (result.isConfirmed) {
-      if (onConfirm) {
-        await onConfirm()
+  const showConfirmBorrowStatus = async (dueDate, onConfirm) => {
+    return await showAlert({
+      title: `This book has been reserved. Due date: ${dueDate}`,
+      text: "Do you want to pick up this book now?",
+      position: 'top-end',
+      theme: themeStore.theme,
+      backdrop: backdropColor,
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+      denyButtonText: "No",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        if (onConfirm) {
+          await onConfirm()
+        }
+        Swal.fire("Success!", 'Book picked up.', "success")
+      } else if (result.isDenied) {
+        Swal.fire("Reserved!", 'Book is reserved.', "info")
       }
-      Swal.fire("Success!", 'Book picked up.', "success")
-    } else if (result.isDenied) {
-      Swal.fire("Reserved!", 'Book is reserved.', "info")
-    }
-  })
-}
+    })
+  }
 
   return { showError, showSuccess, showConfirm, showBorrowed, showConfirmBorrowStatus }
 }
